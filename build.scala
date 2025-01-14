04760c0048c8d0cf9d9116ed9d60296bc210b3e7
bjornregnell <bjorn.regnell@cs.lth.se>
2025-01-14 14:27:16 +0100

    update build

--- build.scala
+++ build.scala
@@ -1,9 +1,18 @@
 //> using scala 3.3.4
 //> using toolkit 0.6.0
-// run with: scala run . -M build
-@main def build = 
-  // https://pandoc.org/demos.html
-  println("TODO: use pandoc to build with os.proc")
-  println("Meanwhile use these commands: ")
-  println("pandoc footer.md -o footer.html")
-  println("pandoc -s --toc -c pandoc.css -A footer.html index.md -o index.html")
+/* 
+  run with: 
+    scala run . -M build
+  or click on run above def build
+*/ 
+extension (s: String) def toProc = os.proc(s.split(" ").toSeq)
+
+val buildFooter = "pandoc footer.md -o footer.html"
+val buildIndex  = "pandoc -s --toc -c pandoc.css -A footer.html index.md -o index.html"
+
+@main def build = // https://pandoc.org/demos.html
+  println("Using https://pandoc.org to build with Scala toolkit os.proc")
+  println(buildFooter)
+  buildFooter.toProc.call()
+  println(buildIndex)
+  buildIndex.toProc.call()
